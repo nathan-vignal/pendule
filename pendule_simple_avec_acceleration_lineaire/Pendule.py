55eb1ae90529865a1d9fb267f17cbbeabf79abd3
theo-poujol <theo.poujol@etu.univ-amu.fr>
2018-11-07 21:13:21 +0100

    Correction fautes pendules.py

--- pendule_simple_avec_acceleration_lineaire/Pendule.py
+++ pendule_simple_avec_acceleration_lineaire/Pendule.py
@@ -7,7 +7,7 @@ def createTranslation(tx, ty) :
     matrice = [[1, 0, tx], [0, 1, ty], [0, 0, 1]]
     return matrice
 
-#récupère sois la valeur minimale sois la valeur max qu'a value le paramètre suivis(celui affiché dans le graphique)
+# Récupère soit la valeur minimale soit la valeur maximale qu'a value le paramètre suivi (celui affiché dans le graphique)
 def getextreme(liste, getMin):
     if(getMin):
         extremum = 9999999
@@ -25,22 +25,24 @@ class Pendule:
         self.name = name
         self.g = g
         self.l = l
-        self.k = k #round(6*pi*(1/1000)*50,4)        #6*pi*(coeffvisqositémillieu)*rayonboule
+        self.k = k # round(6*pi*(1/1000)*50,4)  #6*pi*(coeff de viscosité du millieu)*rayon de la boule
         self.theta = round(theta,4)
         self.masse = masse
-        #période
+
+        # Période
         self.T = 2 * pi * sqrt(self.l / self.g) * (1 + (self.theta * self.theta) / 16) # formule borda
+
         self.color = color
         self.listeGraph = []
         self.valeurGraphMin = 0
         self.valeurGraphMax = 0
         self.deltaExtremum = 0
-        if origine == 'not defined':  # permet un parametre par défaut pour origine en fonction de l
+        if origine == 'not defined':  # Permet un parametre par défaut pour l'oorigine en fonction de l (longueur du fil)
             self.origine = (0, l)
         else:
             self.origine = (origine,l)
 
-    #affiche dans le terminale les infos du pendule
+    #Affiche dans le terminal les infos du pendule
     def show(self):
         print('----' + self.name + '----')
         print('g = ' + str(self.g) + 'm/s^2')
@@ -66,7 +68,7 @@ class Pendule:
         return matriceC
 
 
-    #fait une rotation par rapport à la fixation du pendule
+    # Effectue une rotation par rapport à la fixation du pendule
     def rotate(self, t, dernierCoordonne,):
         # parametre
         translation = [[1, 0, -self.origine[0]],
@@ -84,76 +86,83 @@ class Pendule:
         return dernierCoordonne[0]
 
 
-    #calcul toute les positions du pendule tant qu'il a un minimum de vitesse
+    # Calcule toutes les positions du pendule tant qu'il a un minimum de vitesse
     def simulate(self, tempsEntreImages, ):  # but : renvoyer une liste avec les positions en fonctions du temps
 
 
         actualTheta = self.theta
         previousTheta = actualTheta
 
-        #upperTheta représente le theta à chaque fin d'un aller (ex: pi/2 au début puis -pi/2-frottement)
+        # upperTheta représente le theta à chaque fins d'un aller (ex: pi/2 au début puis -pi/2-frottement)
         upperTheta = actualTheta
         upperPreviousTheta = 9999
 
 
-        # calcul premiere position
+        # Calcule la  première position
         liste = [[self.origine[0]+ sin(actualTheta)* self.l , (self.origine[1] - cos(actualTheta)* self.l) , 1]]
-        # initialise le booléen :(pendule à droite) du coté opposé pour des raisons de programme
+
+        # Initialise le booléen :(pendule à droite) du côté opposé pour des raisons de programme
         if (liste[len(liste) - 1][0] > self.origine[0]):
             penduleADroite = 0
         else:
             penduleADroite = 1
-        # tant que le pendule bouge encore à une vitesse
+        # Tant que le pendule bouge encore à une certaine vitesse
         while (1):
             print('début')
-            # gestion de la fin du bloc aller (dépend du coté du pendule)
-            if (liste[len(liste) - 1][0] > self.origine[0]): #si le pendule est à droite
-                if(penduleADroite):   # est qu'il était déjà à droite
-                    break  #arrête la simulation
+
+            # Gestion de la fin du bloc aller (dépend du coté du pendule)
+
+            if (liste[len(liste) - 1][0] > self.origine[0]): # Si le pendule est à droite
+                if(penduleADroite):   # Et qu'il était déjà à droite
+                    break  # Arrêt de la simulation
                 penduleADroite = 1
-            else:                       #même chose coté gauche
+            else:                       # Même chose du coté gauche
                 if (not penduleADroite):
                     break
                 penduleADroite = 0
 
             upperPreviousTheta = upperTheta
             actualTheta = upperTheta
-            ########## bloc gérant un aller#############
+
+            ########## Bloc gérant un aller#############
             temps = tempsEntreImages
             vitesse = 0
             i = 0
             while (1) :
+
                 # vitesse  =  vitesse + accélération * temps entre image
                 vitesse += (-((self.g / self.l) * sin(actualTheta)) - (self.k / (self.masse * (
                         self.l * self.l))) * vitesse) * tempsEntreImages
-                #nouvel angle = ancien angle + vitesseAngulaire * temps entre images
+
+                # Nouvel angle = ancien angle + vitesseAngulaire * temps entre images
                 actualTheta += vitesse * tempsEntreImages
-                #delta est la rotation a effectué pour avoir la nouvelle position du pendule
+
+                # Delta est la rotation à effectuer pour avoir la nouvelle position du pendule
                 delta = actualTheta - previousTheta
                 previousTheta = actualTheta
 
                 temps += tempsEntreImages
-                #si la vitesse relativement petite  (par rapport au temps entre image et à la longeur du cable) en fonction du côté
+                # Si la vitesse est relativement petite  (par rapport au temps entre image et à la longeur du fil) en fonction du côté
                 # car la vitesse est négative de droite à gauche, et l'opposé de gauche à droite: la vitesse angulaire est dans le sens trigonométrique
                 if (not (((vitesse <= -0.05*tempsEntreImages/self.l) and penduleADroite) or ((vitesse > 0.05*tempsEntreImages/self.l) and (not penduleADroite)))):
                     upperTheta = actualTheta
                     break
                 else:
-                    #si la vitesse est assé grande on rajoute la position du pendule à la liste des position
-                    liste.append(self.rotate(delta, [liste[len(liste) - 1]]))# nouvelle position = ancienne tourné de la différence d'angle entre les deux, par rapport à la fixation
+                    # Si la vitesse est assez grande on rajoute la position du pendule à la liste des position
+                    liste.append(self.rotate(delta, [liste[len(liste) - 1]]))#  Nouvelle position = ancienne position tournée de la différence d'angle entre les deux, par rapport à la fixation
 
                     self.listeGraph.append(vitesse)
             ########## fin bloc gérant un aller#############
-        #####fin calcul de position####
+        ##### fin calcul de position####
 
 
 
 
 
-        #après avoir calculer toute les positions de pendule
-        #récupère les extrème des valeurs à affiché sur le graphique
+        # Après avoir calculé toutes les positions du pendule
+        # Récupère les extrêmes des valeurs à afficher sur le graphique
         self.valeurGraphMin = getextreme(self.listeGraph, 1)
         self.valeurGraphMax = getextreme(self.listeGraph, 0)
         self.deltaExtremum = self.valeurGraphMax - self.valeurGraphMin
-        #on renvoie toute les positions
+        # On renvoie toutes les positions
         return liste
