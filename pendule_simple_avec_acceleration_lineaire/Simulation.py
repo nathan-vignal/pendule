080896047e0e4c1e9fa6c7a10509bb97b92e4c38
theo-poujol <theo.poujol@etu.univ-amu.fr>
2018-11-07 21:22:23 +0100

    Correction fautes Simulation.py

--- pendule_simple_avec_acceleration_lineaire/Simulation.py
+++ pendule_simple_avec_acceleration_lineaire/Simulation.py
@@ -9,28 +9,28 @@ import Graphique
 class Simulation:
 
     def __init__(self, pendules, tempsEntreImages):
-        #liste qui va acceuillir la liste de déplacement de chaque pendule
+        # Liste qui va accueillir la liste de déplacement de chaque pendule
         self.listeDeplacement = []
         #liste des pendules de la simulation
         self.pendules = pendules
         self.tempsEntreImages = tempsEntreImages
 
-        #on vérifie que pour chaque pendule le temps de faire un aller n'est pas inférieur au temps entre image
+        # On vérifie que pour chaque pendules le temps de faire un aller n'est pas inférieur au temps entre images
         for pendule in pendules:
             if (pendule.T/2 <= tempsEntreImages or tempsEntreImages > 0):
                 print('merci de ne pas faire n\'importe quoi et de mettre un temps entre image raisonable')
 
-    #affiche les infos des pendules de la simulation
+    # Affiche les infos des pendules de la simulation
     def recap(self):
         for pendule in self.pendules:
             pendule.show()
 
-    # calcul tout les déplacements de chaque pendules
+    # Calcule tous les déplacements de chaque pendules
     def simulate(self):
         for j in range(0, len(self.pendules), 1):
             self.listeDeplacement.append(self.pendules[j].simulate(self.tempsEntreImages))
 
-    #affiche l'animation
+    # Affiche l'animation
     def show(self):
         display = Display.Display()
         display.display(self)
